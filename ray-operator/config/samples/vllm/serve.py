43feded5c2e837ed4e1943535babe429bcc39abd
Zhikun Lao <zkl94@users.noreply.github.com>
2025-02-20 23:04:54 -0500

    Update serve.py
    Signed-off-by: Zhikun Lao <zkl94@users.noreply.github.com>

--- ray-operator/config/samples/vllm/serve.py
+++ ray-operator/config/samples/vllm/serve.py
@@ -160,7 +160,7 @@ def build_app() -> serve.Application:
         "model": os.environ['MODEL_1_ID'],
         "tensor_parallel_size": int(os.environ['MODEL_1_TENSOR_PARALLELISM']),
         # optional quantization
-        # "quantize": os.environ.get('MODEL_1_QUANTIZE', None),
+        "quantization": os.environ.get('MODEL_1_QUANTIZE', None),
     }
     models_handles[os.environ['MODEL_1_ID']] = VLLMDeployment.options(
         ray_actor_options={"num_cpus": 4}).bind(**model_1_kwargs)
@@ -170,7 +170,7 @@ def build_app() -> serve.Application:
         "model": os.environ['MODEL_2_ID'],
         "tensor_parallel_size": int(os.environ['MODEL_2_TENSOR_PARALLELISM']),
         # optional quantization
-        # "quantize":  os.environ.get('MODEL_2_QUANTIZE', None),
+        "quantization":  os.environ.get('MODEL_2_QUANTIZE', None),
     }
     models_handles[os.environ['MODEL_2_ID']] = VLLMDeployment.options(
         ray_actor_options={"num_cpus": 4}).bind(**model_2_kwargs)
